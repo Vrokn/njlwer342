950aeccc5c67b6b4b9338ea92a6495637eff4fb8
Mateo <mgomezossa@gmail.com>
2020-05-06 21:27:01 -0500

    create list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 
-/// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [
+        'Sacar la ropa',
+        'Hacer la cama',
+        'Leer un rato',
+      ],
+      newTask: '',
+      vacio: '',
+    }
+  }
+  UpdateValue = (event) => {
+    this.setState({
+      newTask: event.target.value,
+    });
+  }
+  addTask = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.setState({ tasks: this.state.tasks.concat(this.state.newTask) })
+    }
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    const vacio = this.state.vacio;
+    this.props.onSearchTermChange(vacio);
+    this.setState({ vacio: '' })
+  };
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+
+            {this.state.tasks.map(item => (
+              <li>{item}</li>)
+            )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.vacio} onSubmit={(event) => this.handleSubmit(event)} onChange={(event) => this.UpdateValue(event)} onKeyPress={this.addTask.bind(this)} />
+          </form>
         </div>
       </div>
     )
